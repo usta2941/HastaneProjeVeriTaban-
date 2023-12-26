63820b1c72ea090beec076f35143a2f2cf175de6
Abdulkadir_USTA <usta1829@gmail.com>
2023-12-26 17:13:46 +0300

    Add project files.

---
+++ HastaneProje/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace HastaneProje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.hastaIsım_text = new System.Windows.Forms.TextBox();
+            this.RefakatcSoyad_tex = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.RefakatciIsım_text = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.hastaSoyad_text = new System.Windows.Forms.TextBox();
+            this.DoktorIsım_text = new System.Windows.Forms.TextBox();
+            this.DoktorSoyad_text = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.hastaID_text = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(444, 390);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(698, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(698, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // hastaIsım_text
+            // 
+            this.hastaIsım_text.AccessibleName = "";
+            this.hastaIsım_text.Location = new System.Drawing.Point(460, 78);
+            this.hastaIsım_text.Name = "hastaIsım_text";
+            this.hastaIsım_text.Size = new System.Drawing.Size(100, 22);
+            this.hastaIsım_text.TabIndex = 4;
+            // 
+            // RefakatcSoyad_tex
+            // 
+            this.RefakatcSoyad_tex.AccessibleName = "";
+            this.RefakatcSoyad_tex.Location = new System.Drawing.Point(460, 229);
+            this.RefakatcSoyad_tex.Name = "RefakatcSoyad_tex";
+            this.RefakatcSoyad_tex.Size = new System.Drawing.Size(100, 22);
+            this.RefakatcSoyad_tex.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(457, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Hasta İsim";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(457, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Hasta Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(457, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Refakatçi İsim";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(457, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Refakatçi Soyad";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(698, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Listele";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // RefakatciIsım_text
+            // 
+            this.RefakatciIsım_text.AccessibleName = "";
+            this.RefakatciIsım_text.Location = new System.Drawing.Point(460, 174);
+            this.RefakatciIsım_text.Name = "RefakatciIsım_text";
+            this.RefakatciIsım_text.Size = new System.Drawing.Size(100, 22);
+            this.RefakatciIsım_text.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(457, 265);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Doktor İsim";
+            // 
+            // hastaSoyad_text
+            // 
+            this.hastaSoyad_text.AccessibleName = "";
+            this.hastaSoyad_text.Location = new System.Drawing.Point(460, 122);
+            this.hastaSoyad_text.Name = "hastaSoyad_text";
+            this.hastaSoyad_text.Size = new System.Drawing.Size(100, 22);
+            this.hastaSoyad_text.TabIndex = 17;
+            // 
+            // DoktorIsım_text
+            // 
+            this.DoktorIsım_text.AccessibleName = "";
+            this.DoktorIsım_text.Location = new System.Drawing.Point(460, 300);
+            this.DoktorIsım_text.Name = "DoktorIsım_text";
+            this.DoktorIsım_text.Size = new System.Drawing.Size(100, 22);
+            this.DoktorIsım_text.TabIndex = 18;
+            // 
+            // DoktorSoyad_text
+            // 
+            this.DoktorSoyad_text.AccessibleName = "";
+            this.DoktorSoyad_text.Location = new System.Drawing.Point(460, 368);
+            this.DoktorSoyad_text.Name = "DoktorSoyad_text";
+            this.DoktorSoyad_text.Size = new System.Drawing.Size(100, 22);
+            this.DoktorSoyad_text.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(457, 337);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 16);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Doktor Soyad";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(461, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 16);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Hasta ID";
+            // 
+            // hastaID_text
+            // 
+            this.hastaID_text.AccessibleName = "";
+            this.hastaID_text.Location = new System.Drawing.Point(464, 33);
+            this.hastaID_text.Name = "hastaID_text";
+            this.hastaID_text.Size = new System.Drawing.Size(100, 22);
+            this.hastaID_text.TabIndex = 22;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(698, 218);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "Güncelle";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.hastaID_text);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.DoktorSoyad_text);
+            this.Controls.Add(this.DoktorIsım_text);
+            this.Controls.Add(this.hastaSoyad_text);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.RefakatciIsım_text);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RefakatcSoyad_tex);
+            this.Controls.Add(this.hastaIsım_text);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox hastaIsım_text;
+        private System.Windows.Forms.TextBox RefakatcSoyad_tex;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox RefakatciIsım_text;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox hastaSoyad_text;
+        private System.Windows.Forms.TextBox DoktorIsım_text;
+        private System.Windows.Forms.TextBox DoktorSoyad_text;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox hastaID_text;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ HastaneProje/Form1.cs
@@ -0,0 +1,74 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HastaneProje
+{
+    public partial class Form1 : Form
+    {
+        NpgsqlConnection baglanti = new NpgsqlConnection("server=localHost;port=5432;Database=hastaneOtomasyon; user ID=postgres;password=1234");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // hasta ekleme
+        {
+            
+           baglanti.Open();
+            NpgsqlCommand komut1 = new NpgsqlCommand("insert into personel (personelNo,adi,soyadi,personelTipi) values(@p1,@p2,@p3,@p4)", baglanti);
+            komut1.Parameters.AddWithValue("@p1", int.Parse(hastaID_text.Text));
+            komut1.Parameters.AddWithValue("@p2", DoktorIsım_text.Text);
+            komut1.Parameters.AddWithValue("@p3", DoktorSoyad_text.Text);
+            string doktor = "doktor";
+            komut1.Parameters.AddWithValue("@p4", doktor);
+            komut1.ExecuteNonQuery(); 
+            baglanti.Close();
+            baglanti.Open();
+            NpgsqlCommand komut2 = new NpgsqlCommand("insert into doktor(personelNo,departmanID) values(@p1,@p2)", baglanti);
+            komut2.Parameters.AddWithValue("@p1", int.Parse(hastaID_text.Text));
+            komut2.Parameters.AddWithValue("@p2", int.Parse("1"));
+            komut2.ExecuteNonQuery();
+            baglanti.Close();
+            baglanti.Open();
+            NpgsqlCommand komut3 = new NpgsqlCommand("insert into hasta(id,ad,soyad,doktorID) values(@p1,@p2,@p3,@p4)", baglanti);
+            komut3.Parameters.AddWithValue("@p1", int.Parse(hastaID_text.Text));
+            komut3.Parameters.AddWithValue("@p2", hastaIsım_text.Text);
+            komut3.Parameters.AddWithValue("@p3", hastaSoyad_text.Text);
+            komut3.Parameters.AddWithValue("@p4", int.Parse(hastaID_text.Text));
+            komut3.ExecuteNonQuery();
+            baglanti.Close();
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)// listele
+        {
+            baglanti.Open();
+            string sorgu = "select * from hasta";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu, baglanti);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            baglanti.Close();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
